aa3dc714afb4ab0e94f477bb13033cdb4b5e2beb
thirumala <thirumala@192.168.1.120>
2016-09-01 21:09:43 +0400

    2nd commit

--- TestProject/src/com/test/Test.java
+++ TestProject/src/com/test/Test.java
@@ -2,7 +2,8 @@ package com.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test......");
+		System.out.println("Test......1");
+		System.out.println("Test......2");
 		  
 	}
 
